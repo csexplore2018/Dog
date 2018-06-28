3d6b7bf6b84d7f066ea1c893d516bc79dd0723fc
gc_science13 <gc_science13@SE137LAB-27.ad.svsu.edu>
2018-06-28 10:45:10 -0400

    2nd Commit; Created Dogg class with Bitch and Sire extentions

--- src/dog/Dog.java
+++ src/dog/Dog.java
@@ -11,11 +11,39 @@ package dog;
  */
 public class Dog {
 
+    public String name= "Dog";
+    public String food="Bone";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+       Animal doggoA= new Animal();
+       doggoA.talk();
+       
+              Bitch doggoB= new Bitch();
+       doggoB.talk();
+       
+              Sire doggoC= new Sire();
+       doggoC.talk();
+    }  
 }
+
+class Animal {
+
+    public String name = "Animal";
+    public String food="Food";
+
+    public void talk(){ System.out.println("Hey.");  }
+}
+
+
+
+class Bitch extends Animal{
+ public void talk(){ System.out.println("Woof");}
+}
+
+class Sire extends Bitch {
+    public String name = " Doggo";
+}
+
